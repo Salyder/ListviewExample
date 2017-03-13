26a3edfd64e38bc5a12b34133ae77a29ca87f983
Salyder <bml998@live.com>
2017-03-12 21:31:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brandonmain/listviewexample1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.brandonmain.listviewexample1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import static android.R.attr.name;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView resultsListView = (ListView) findViewById(R.id.results_listview);
+
+        HashMap<String, String> nameAddresses = new HashMap<>();
+        nameAddresses.put("Diana", "3214 Broadway Avenue");
+        nameAddresses.put("Tyga", "343 Rack City Drive");
+        nameAddresses.put("Rich Homie Quan", "111 Everything Gold Way");
+        nameAddresses.put("Donna", "789 Escort St");
+        nameAddresses.put("Bartholomew", "332 Dunkin St");
+        nameAddresses.put("Eden", "421 Angelic Blvd");
+
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item,
+                new String[]{"First Line", "Second Line"},
+                new int[]{R.id.text1, R.id.text2});
+
+
+        Iterator it = nameAddresses.entrySet().iterator();
+        while (it.hasNext())
+        {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        resultsListView.setAdapter(adapter);
+
+    }
+}
